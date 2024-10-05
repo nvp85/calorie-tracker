7c8cdec7d9663afe61a647c89adc78ddc84d3391
nvp85 <nvp@zhukov.al>
2024-10-05 15:32:42 -0500

    modify styles in LandingPage.css

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -7,7 +7,9 @@ export default function LandingPage() {
         <div className="hero-container">
             <h1>Track Your Calories, Reach Your Goals</h1>
             <p>Say goodbye to guesswork and hello to a healthier you! </p>
-            <p>Our minimalistic calorie tracker makes it easy to log meals, track nutrients, and stay on top of your goals. Start tracking today for a happier, healthier tomorrow!</p>
+            <p>Our minimalistic calorie tracker makes it easy to log meals, track nutrients, and stay on top of your goals. 
+                With a simple, user-friendly interface, you’ll have all the tools you need to eat better and feel great. 
+                Start tracking today for a happier, healthier tomorrow!</p>
             <button className="start-btn">Get started</button>
         </div>
     )
