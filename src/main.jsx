0503943c0776ccf526007363188d23143a519a03
nvp85 <nvp@zhukov.al>
2024-10-07 16:18:40 -0500

    set up a Mirage server to mock api calls in development

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 //import './index.css';
+import { makeServer } from "./server"
+
+makeServer();
 
 
 createRoot(document.getElementById('root')).render(
