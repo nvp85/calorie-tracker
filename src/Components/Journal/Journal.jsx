ad03bea9e92adb0c9cc3057269c843089567f934
nvp85 <nvp@zhukov.al>
2024-10-17 23:19:27 -0500

    add a summary into the journal

--- src/Components/Journal/Journal.jsx
+++ src/Components/Journal/Journal.jsx
@@ -7,15 +7,26 @@ import { Link } from "react-router-dom";
 
 export default function Journal() {
     const [records, setRecords] = useOutletContext();
+    let total = {calories: 0, proteins: 0, fats: 0, carbs: 0};
+    for (const record of records) {
+        total.calories += record.calories_eaten;
+        total.proteins += record.protein_eaten;
+        total.fats += record.fat_eaten;
+        total.carbs += record.carbs_eaten;
+    };
     return (
         <div className="white-container">
             <h2>You have eaten today:</h2>
+            <div className="summary"> 
+                <p>Calories, total: {total.calories} Ccal</p>
+                <p>Proteins: {total.proteins} g | Fats: {total.fats} g | Carbs: {total.carbs} g</p>
+            </div>
             {records.map(record => {
                 return (
                     <div className="record" key={record.id}>
                         <div>
                         <p>{record.food_name}</p>
-                        <p>Calories:{record.calories_eaten} | Amount: {record.amount} g.</p>
+                        <p>Calories: {record.calories_eaten} | Amount: {record.amount} g.</p>
                         </div>
                         <Link to={`/food/${record.food_id}`} className="food-select-btn"><LuPlus className="add-icon"/></Link>
                     </div>
