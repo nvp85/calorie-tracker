641ebd71a9c08cc54506f7e65f0197a9c98bfe41
nvp85 <nvp@zhukov.al>
2024-10-17 23:21:59 -0500

    add the logging functionality

--- src/Components/FoodItem/FoodItem.jsx
+++ src/Components/FoodItem/FoodItem.jsx
@@ -1,17 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./FoodItem.css";
+import { useOutletContext, useNavigate } from "react-router-dom";
 
 
 export default function FoodItem() {
     const params = useParams();
     const [item, setItem]  = useState(null);
+    const [records, setRecords] = useOutletContext();
+    const [amount, setAmount] = useState(0);
 
     useEffect(() => {
         fetch(`/api/food/${params.id}`)
             .then(res => res.json())
             .then(data => setItem(data))
     }, [params.id]);
+
+    let navigate = useNavigate();
+
+    function isValid(amount) {
+        const pattern = /^\d+(\.\d+)?$/;
+        return pattern.test(amount);
+    };
+
+    function logFood () {
+        const userId = "1"; // hardcoded for now
+        const cur_date = new Date().toJSON().slice(0,10);
+        if (isValid(amount)) {
+            const newRecord = {
+                userId: userId, 
+                foodId: item.id, 
+                amount: Number(amount),
+                date: cur_date,
+            };
+            fetch(`/api/users/${userId}/food_records`, {
+                method: "POST",
+                body: JSON.stringify(newRecord)
+            })
+            .then(res => res.json())
+            .then(data => {
+                setRecords(old => [data, ...old]);
+                navigate("/journal");
+            })
+        } else {
+            alert("Please, enter a number of grams");
+        }
+    };
 
     return (
         <div className="white-container">
@@ -20,10 +54,10 @@ export default function FoodItem() {
             <h2>Log the food</h2>
             <p>{item.name}</p>
             <p>Calories, per 100g: {item.calories}, Serving size: {item.serving}</p>
-            <p>Protein: {item.proteins} | Fat: {item.fats} | Carbohydrates: {item.carbs}</p>
+            <p>Proteins: {item.proteins} g | Fats: {item.fats} g | Carbs: {item.carbs} g</p>
             <label htmlFor="amount">Enter amount</label>
-            <input type="text" id="amount"/>
-            <button>Log</button>
+            <input type="text" id="amount" name="amount" onChange={(e) => setAmount(e.target.value)}/>
+            <button onClick={logFood}>Log</button>
             </div>
             : <p>Loading...</p>
             }
--- src/server.js
+++ src/server.js
@@ -31,18 +31,22 @@ export function makeServer() {
                 include: ['food', 'user'],
                 serialize(resource, request) {
                     let json = serializer.prototype.serialize.apply(this, arguments);
-                    return json.map(record => ({
-                        id: record.id,
-                        amount: record.amount,
-                        date: record.date,
-                        user_id: record.user.id,
-                        food_id: record.food.id,
-                        food_name: record.food.name,
-                        calories_eaten: Math.round(parseInt(record.amount)*parseInt(record.food.calories)/100),
-                        protein_eaten: Math.round(parseInt(record.amount)*parseInt(record.food.proteins)/100),
-                        fat_eaten: Math.round(parseInt(record.amount)*parseInt(record.food.fats)/100),
-                        carbs_eaten: Math.round(parseInt(record.amount)*parseInt(record.food.carbs)/100)
-                    }))
+                    function customRecord(record) {
+                        return {
+                            id: record.id,
+                            amount: record.amount,
+                            date: record.date,
+                            user_id: record.user.id,
+                            food_id: record.food.id,
+                            food_name: record.food.name,
+                            calories_eaten: Math.round(record.amount * record.food.calories / 100),
+                            protein_eaten: Math.round(record.amount * record.food.proteins / 100),
+                            fat_eaten: Math.round(record.amount * record.food.fats / 100),
+                            carbs_eaten: Math.round(record.amount * record.food.carbs / 100),
+                        }
+                    };
+                    json = Array.isArray(json) ? json.map(record => customRecord(record)) : customRecord(json);
+                    return json;
                 }
             }),
             food: serializer.extend({
@@ -52,29 +56,29 @@ export function makeServer() {
 
         seeds(server) {
             //users
-            server.create("user", {id:"1", username: "demoUser", email: "user1@example.com", password: "123", budget: "2000"});
-            server.create("user", {id:"2", username: "demoUser2", email: "user2@example.com", password: "456", budget: "2000"});
+            server.create("user", {id: 1, username: "demoUser", email: "user1@example.com", password: "123", budget: 2000});
+            server.create("user", {id: 2, username: "demoUser2", email: "user2@example.com", password: "456", budget: 2000});
 
             //food public
-            server.create("food", {id: "1", name: "Chicken egg", calories: "157.0", proteins: "12.7", fats: "10.9", carbs: "0.7", serving: "55g", addedById: null});
-            server.create("food", {id: "2", name: "Bananas", calories: "95.0", proteins: "1.5", fats: "0.2", carbs: "21.8", serving: "110g", addedById: null});
-            server.create("food", {id: "3", name: "Milk whole", calories: "59.0", proteins: "2.9", fats: "3.2", carbs: "4.7", serving: "1 cup (225g)", addedById: null});
-            server.create("food", {id: "4", name: "Milk 1.5%", calories: "44.0", proteins: "2.8", fats: "1.5", carbs: "4.7", serving: "1 cup (255g)", addedById: null});
-            server.create("food", {id: "5", name: "Wheat Bread", calories: "242.0", proteins: "25", fats: "32", carbs: "0", serving: "28g", addedById: null});
+            server.create("food", {id: 1, name: "Chicken egg", calories: 157.0, proteins: 12.7, fats: 0.9, carbs: 0.7, serving: "55g", addedById: null});
+            server.create("food", {id: 2, name: "Bananas", calories: 95.0, proteins: 1.5, fats: 0.2, carbs: 21.8, serving: "110g", addedById: null});
+            server.create("food", {id: 3, name: "Milk whole", calories: 59.0, proteins: 2.9, fats: 3.2, carbs: 4.7, serving: "1 cup (225g)", addedById: null});
+            server.create("food", {id: 4, name: "Milk 1.5%", calories: 44.0, proteins: 2.8, fats: 1.5, carbs: 4.7, serving: "1 cup (255g)", addedById: null});
+            server.create("food", {id: 5, name: "Wheat Bread", calories: 242.0, proteins: 25, fats: 32, carbs: 0, serving: "28g", addedById: null});
             
             // users' food
-            server.create("food", {id: "6", name: "Crackers", calories: "437.5", proteins: "6.3", fats: "10", carbs: "75", serving: "16g", addedById: "1"});
-            server.create("food", {id: "7", name: "Sharp cheddar cheese", calories: "392.9", proteins: "1.5", fats: "0.2", carbs: "21.8", serving: "110g", addedById: "1"});
+            server.create("food", {id: 6, name: "Crackers", calories: 437.5, proteins: 6.3, fats: 10, carbs: 75, serving: "16g", addedById: 1});
+            server.create("food", {id: 7, name: "Sharp cheddar cheese", calories: 392.9, proteins: 1.5, fats: 0.2, carbs: 21.8, serving: "110g", addedById: 1});
 
             
             //records
-            server.create("foodRecord", {id:"1", userId: "1", foodId: "5", amount: "200", date: cur_date});
-            server.create("foodRecord", {id:"2", userId: "1", foodId: "1", amount: "110", date: cur_date});
-            server.create("foodRecord", {id:"3", userId: "1", foodId: "4", amount: "20", date: cur_date});
-            server.create("foodRecord", {id:"4", userId: "1", foodId: "5", amount: "120", date: cur_date});
-            server.create("foodRecord", {id:"5", userId: "1", foodId: "2", amount: "300", date: cur_date});
-            server.create("foodRecord", {id:"6", userId: "1", foodId: "3", amount: "105", date: cur_date});
-            server.create("foodRecord", {id:"7", userId: "2", foodId: "3", amount: "216", date: cur_date});
+            server.create("foodRecord", {id: 1, userId: 1, foodId: 5, amount: 200, date: cur_date});
+            server.create("foodRecord", {id: 2, userId: 1, foodId: 1, amount: 110, date: cur_date});
+            server.create("foodRecord", {id: 3, userId: 1, foodId: 4, amount: 20, date: cur_date});
+            server.create("foodRecord", {id: 4, userId: 1, foodId: 5, amount: 120, date: cur_date});
+            server.create("foodRecord", {id: 5, userId: 1, foodId: 2, amount: 300, date: cur_date});
+            server.create("foodRecord", {id: 6, userId: 1, foodId: 3, amount: 105, date: cur_date});
+            server.create("foodRecord", {id: 7, userId: 2, foodId: 3, amount: 216, date: cur_date});
 
         },
     // TODO: add brands and categories
@@ -90,7 +94,12 @@ export function makeServer() {
                 return schema.foods.find(request.params.id);
             });
             this.get("/users/:id/food_records/:date", (schema, request) => {
-                return schema.foodRecords.where({userId: request.params.id, date: request.params.date})
+                return schema.foodRecords.where({userId: request.params.id, date: request.params.date}).sort((a,b) => b.id - a.id);
+            });
+            this.post("/users/:id/food_records", (schema, request) => {
+                let attrs = JSON.parse(request.requestBody);
+                console.log(attrs);
+                return schema.foodRecords.create(attrs);
             });
         },
     })
