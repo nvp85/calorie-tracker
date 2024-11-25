750347807794fc2051ca8c8e1c1605a80b85af76
nvp85 <nvp@zhukov.al>
2024-11-24 18:51:30 -0600

    add a link to the logo

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import './LandingPage.css'
+import './LandingPage.css';
+import { Link } from "react-router-dom";
 
 
 export default function LandingPage() {
@@ -10,7 +11,7 @@ export default function LandingPage() {
             <p>Our minimalistic calorie tracker makes it easy to log meals, track nutrients, and stay on top of your goals. 
                 With a simple, user-friendly interface, you’ll have all the tools you need to eat better and feel great. 
                 Start tracking today for a happier, healthier tomorrow!</p>
-            <button className="start-btn orange btn">Get started</button>
+            <Link to="/signup" className="start-btn orange btn">Get started</Link>
         </div>
     )
 }
