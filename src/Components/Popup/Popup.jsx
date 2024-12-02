7b52432819ba570da54a4daba7384f5dba2b8e0a
nvp85 <nvp@zhukov.al>
2024-12-01 23:27:56 -0600

    add styles to the popup's close button

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import "./Popup.css"
+import "./Popup.css";
+import { LuX } from "react-icons/lu";
+
 
 export default function Popup({ children, onClose }) {
   return (
       <div className="popup-overlay" onClick={onClose}>
-        <div className="popup-content" onClick={e => e.stopPropagation()}>
+        <div className="popup-content position-relative" onClick={e => e.stopPropagation()}>
           {children}
-          <button onClick={onClose}>Close</button>
+          <button onClick={onClose} className='close-btn'><LuX /></button>
         </div>
       </div>
   );
